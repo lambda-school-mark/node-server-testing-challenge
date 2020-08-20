6d6d1387b3baace5ef88272084a752fb76760cc7
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-20 19:29:04 -0400

    mvp

--- src/api/users/users.spec.js
+++ src/api/users/users.spec.js
@@ -6,6 +6,9 @@ const db = require("../../db/config");
 describe("server endpoints", () => {
   beforeEach(async () => {
     await db("users").truncate();
+    await request(server)
+      .post("/users")
+      .send({ username: "TRUMP", password: "lol" });
   });
 
   describe("POST /users", () => {
@@ -16,18 +19,34 @@ describe("server endpoints", () => {
 
       const users = await db("users");
 
-      expect(users).toHaveLength(1);
+      expect(users).toHaveLength(2);
     });
   });
 
-  describe("", () => {
+  describe("POST /users", () => {
     it("should return 200", async () => {
       await request(server)
-        .post("/")
+        .post("/users")
         .send({ username: "timmy", password: "lol" })
         .then((res) => {
           expect(res.status).toBe(201);
         });
+    });
+  });
+
+  describe("DELETE", () => {
+    it("should delete user", async () => {
+      await request(server)
+        .delete("/users/1")
+        .then((res) => {
+          expect(res.status).toBe(204);
+        });
+    });
+    it("should return content type json", async () => {
+      await request(server)
+        .post("/users")
+        .send({ username: "timmy", password: "lol" })
+        .expect("Content-Type", /json/i);
     });
   });
 });
--- src/server.js
+++ src/server.js
@@ -7,14 +7,20 @@ server.use(express.json());
 
 server.post("/users", (req, res) => {
   Users.add(req.body).then((user) => {
-    res.status(200).json(user);
+    res.status(201).json(user);
   });
 });
 
-server.post("/", (req, res) => {
-  Users.add(req.body).then((user) => {
-    res.status(201).json(user);
-  });
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  Users.remove(id)
+    .then((deleted) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = server;
